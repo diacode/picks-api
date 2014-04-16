1e768dcc434b4a6fa4a812f71e09d5c77d8d6edc
Víctor Viruete <victor@diacode.com>
2014-04-16 13:48:51 +0200

    Added compilation index action to the backend and fixed create action which wasn't returning the proper model

--- app/controllers/api/compilations_controller.rb
+++ app/controllers/api/compilations_controller.rb
@@ -2,6 +2,10 @@ class Api::CompilationsController < Api::BaseController
   respond_to :json
 
   def create
-    respond_with Link.create, location: nil
+    respond_with Compilation.create, location: nil
+  end
+
+  def index
+    respond_with Compilation.all
   end
 end
