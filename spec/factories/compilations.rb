eb477cbc24d8f11a01dbbcc84e9722a06a50f451
Víctor Viruete <victor@diacode.com>
2014-04-16 13:04:12 +0200

    Added compilation spec with generate_installment method test

---
+++ spec/factories/compilations.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :compilation do
+    installment 1
+  end
+end
--- spec/models/compilation_spec.rb
+++ spec/models/compilation_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Compilation do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#generate_installment' do
+    context 'There is no previous compilation registered' do
+      it 'should be the first installment' do
+        compilation = Compilation.new
+        compilation.generate_installment 
+
+        expect(compilation.installment).to eq(1)
+      end
+    end
+
+    context 'There is a previous compilation registered' do
+      let(:previous_compilation){ FactoryGirl.create(:compilation) }
+
+      it 'should be the second installment' do
+        compilation = Compilation.new
+        compilation.generate_installment 
+
+        expect(compilation.installment).to eq(2)
+      end
+    end
+  end
 end
