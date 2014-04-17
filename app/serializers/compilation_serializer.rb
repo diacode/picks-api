5a69a674e88a3b381aa35c0070bcfd559bdf83c5
Víctor Viruete Roldán <victor@diacode.com>
2014-04-17 20:48:02 +0200

    Changed API serializers to serialize associations as array IDs instead of embedded objects

--- app/serializers/compilation_serializer.rb
+++ app/serializers/compilation_serializer.rb
@@ -1,4 +1,5 @@
 class CompilationSerializer < ActiveModel::Serializer
+  embed :ids
   attributes :id, :installment, :published_at, :created_at, :updated_at
   has_many :links
 end
--- app/serializers/link_serializer.rb
+++ app/serializers/link_serializer.rb
@@ -1,4 +1,5 @@
 class LinkSerializer < ActiveModel::Serializer
+  embed :ids
   attributes :id, :url, :title, :description, :category, :status,
     :created_at, :updated_at
   has_one :compilation
