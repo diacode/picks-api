331d37bd6a1079ac29dde5a219996599b3bb5957
Víctor Viruete <victor@diacode.com>
2014-04-17 17:52:44 +0200

    Changed compilation api controller to get link_ids

--- app/controllers/api/compilations_controller.rb
+++ app/controllers/api/compilations_controller.rb
@@ -2,10 +2,14 @@ class Api::CompilationsController < Api::BaseController
   respond_to :json
 
   def create
-    respond_with Compilation.create, location: nil
+    respond_with Compilation.create(compilation_params), location: nil
   end
 
   def index
     respond_with Compilation.all
+  end
+
+  def compilation_params
+    params.require(:compilation).permit(:link_ids)
   end
 end
