68e6a8dc29afbb5d1204c10a10f4e191e64e3b72
Víctor Viruete <victor@diacode.com>
2015-03-04 22:04:03 +0100

    Removed status from link serializer

--- app/serializers/link_serializer.rb
+++ app/serializers/link_serializer.rb
@@ -1,6 +1,5 @@
 class LinkSerializer < ActiveModel::Serializer
   embed :ids
-  attributes :id, :url, :title, :description, :category, :status,
-    :created_at, :updated_at
+  attributes :id, :url, :title, :description, :category, :created_at, :updated_at
   has_one :compilation
 end
