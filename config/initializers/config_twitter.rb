a27cc3133ad64044b630bc5dbbe591b5f0d8debd
Víctor Viruete <victor@diacode.com>
2015-03-05 14:44:08 +0100

    Loading `TWITTER_CONFIG` when `Rails.configuration.tweet_approved_links` is `true`

--- config/initializers/config_twitter.rb
+++ config/initializers/config_twitter.rb
@@ -1,1 +1,3 @@
-TWITTER_CONFIG = YAML.load(ERB.new(File.new("#{Rails.root}/config/twitter.yml").read).result)[Rails.env]
+if Rails.configuration.tweet_approved_links
+  TWITTER_CONFIG = YAML.load(ERB.new(File.new("#{Rails.root}/config/twitter.yml").read).result)[Rails.env]
+end
