c91c34c4e8a14b53e2951598f2ac13cbb0b822c9
Víctor Viruete <victor@diacode.com>
2015-03-08 14:22:18 +0100

    Force processing all requests to api namespace as json

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
   root to: "application#index"
   
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     resources :links
     resources :compilations
   end
