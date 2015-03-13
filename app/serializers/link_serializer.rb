eaecfe87eadf94b302549caab5f74da1e7d160a2
Víctor Viruete <victor@diacode.com>
2015-03-13 20:31:21 +0100

    Added approved attribute to link serializer

--- app/serializers/link_serializer.rb
+++ app/serializers/link_serializer.rb
@@ -1,5 +1,5 @@
 class LinkSerializer < ActiveModel::Serializer
   embed :ids
-  attributes :id, :url, :title, :description, :category, :created_at, :updated_at
+  attributes :id, :url, :title, :description, :category, :created_at, :updated_at, :approved
   has_one :compilation
 end
