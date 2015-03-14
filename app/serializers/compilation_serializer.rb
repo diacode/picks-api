3c1137da4a63b825fb2eacd99ccef8e1019e190f
Víctor Viruete <victor@diacode.com>
2015-03-14 22:20:18 +0100

    Preventing links from being serialized on compilation serializer

--- app/serializers/compilation_serializer.rb
+++ app/serializers/compilation_serializer.rb
@@ -1,4 +1,8 @@
 class CompilationSerializer < ActiveModel::Serializer
-  attributes :id, :installment, :intro, :published_at, :created_at, :updated_at, :publish
+  attributes :id, :installment, :intro, :published_at, :created_at, :updated_at, :publish, :links_count
   has_many :links
+
+  def links_count
+    object.link_ids.count
+  end
 end
