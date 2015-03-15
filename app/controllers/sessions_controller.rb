7c1db23b105ddbf3dd688c3866c1bd69bfc44882
Víctor Viruete <victor@diacode.com>
2015-03-15 14:56:16 +0100

    Improved sessions controller code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,21 +2,25 @@ class SessionsController < Devise::SessionsController
   protect_from_forgery with: :null_session
   
   def create
-    resource = resource_from_credentials
-    data     = {
-      token: resource.api_token,
-      email: resource.email
-    }
-    render json: data, status: 201
+    render response_from_credentials
   end
 
   protected
-    def resource_from_credentials
+    def response_from_credentials
       data = { email: params[:user][:email] }
       if res = resource_class.find_for_database_authentication(data)
         if res.valid_password?(params[:user][:password])
-          res
+          data = {token: res.api_token, email: res.email}
+          {json: data, status: 201}
+        else
+          invalid_response
         end
+      else
+        invalid_response
       end
+    end
+
+    def invalid_response
+      {json: {error: "Invalid Login Details"}, status: 401}
     end
 end
