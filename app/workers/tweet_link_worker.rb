b85ccc87ae8c9c9901dd0018db4c0ddb19c76638
Víctor Viruete <victor@diacode.com>
2015-03-24 12:37:40 +0100

    Improved tweet publishing including desc when it's possible

--- app/workers/tweet_link_worker.rb
+++ app/workers/tweet_link_worker.rb
@@ -1,3 +1,5 @@
+require 'redcarpet/render_strip'
+
 class TweetLinkWorker
   include Sidekiq::Worker
 
@@ -6,6 +8,20 @@ class TweetLinkWorker
   def perform(link_id)
     link = Link.find(link_id) 
     client = Twitter::REST::Client.new(TWITTER_CONFIG)
-    client.update("#{link.title} #{link.url}")
+    client.update(get_tweet_content(link))
+  end
+
+  def get_tweet_content(link)
+    md = Redcarpet::Markdown.new(Redcarpet::Render::StripDown)
+    # We remove any possible markdown markup and strip the ending newline
+    desc = md.render(link.description).strip
+    title_plus_desc = "#{link.title} - #{desc}"
+
+    # Shortened link + space should be ~25 characters
+    if title_plus_desc.length + 25 <= 140
+      "#{title_plus_desc} #{link.url}"
+    else
+      "#{link.title} #{link.url}"
+    end
   end
 end
